ada6fdae449878dc08090651ddecb0617350d18e
Fouad Chmainy <fchmainy@yahoo.com>
2020-08-19 18:42:15 +0200

    change in structure 3

--- variables.tf
+++ variables.tf
@@ -19,11 +19,6 @@ variable "f5vm02int" {}
 variable "f5vm01ext_sec" {}
 variable "f5vm02ext_sec" {}
 
-#Azure SP cred details
-variable "client_id" {default = ""}
-variable "client_secret" {default = ""}
-variable "subscription_id" {default = ""}
-variable "tenant_id" {default = ""}
 
 #other variables
 variable "prefix" {}
